73f374d05031bab2707d11ad0ed8c218746fd1f1
gabi_curteanu82 <gabicurteanu100@gmail.com>
2025-12-06 15:38:16 +0200

    Command and Query interfaces for MediatR IRequest and IRequestHandler to satisfy the CQRS design pattern

---
+++ Backend/AutoWise.CommonUtilities/AutoWise.CommonUtilities.MediatorAbstractions/Cqrs/Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace AutoWise.CommonUtilities.MediatorAbstractions.Cqrs.Commands;
+
+// Unit is a MediatR placeholder return type used when the command doesn't return a value
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
